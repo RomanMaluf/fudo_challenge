55a7a0fb01c8b993c7e95c2d03a21f2ffd4957df
Roman Maluf <eliasmaluf51@gmail.com>
2025-06-01 13:32:26 -0300

    feat: refactor product model due add more readability

--- lib/models/product.rb
+++ lib/models/product.rb
@@ -2,21 +2,19 @@
 
 class Product
   REQUIRED_KEYS = %i[id name].freeze
-  @products = begin
-    JSON.parse(File.read('public/products.json'), symbolize_names: true)
-  rescue StandardError
-    []
-  end
+  @products = JSON.parse(File.read('public/products.json'), symbolize_names: true)
 
-  def self.find(id)
-    @products.find { |product| product[:id].to_s == id.to_s }
-  end
+  class << self
+    def find(id)
+      @products.find { |product| product[:id].to_s == id.to_s }
+    end
 
-  def self.all
-    @products
-  end
+    def all
+      @products
+    end
 
-  def self.add(product)
-    @products << product
+    def add(product)
+      @products << product
+    end
   end
 end
